ae6c4fee969a0a612d045da8bea97e5f0405ad92
Keilin <keilin.1997.aguiar.g@gmail.com>
2022-05-04 22:03:31 +0200

    progreso

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+const date = document.querySelector("#date");
+
+const check = document.querySelector("#enter");
+
+const list = document.querySelector("#list");
+
+const inputAdd = document.querySelector("#newHomework")
+
