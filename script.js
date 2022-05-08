93d7ee6eef193bfe77fb95bc028ef707e0b27936
Keilin <keilin.1997.aguiar.g@gmail.com>
2022-05-08 20:01:11 +0200

    proyecto finalizado

--- script.js
+++ script.js
@@ -2,11 +2,8 @@
 // LA FECHA DEL HEADER
 const dateHeader = document.querySelector('#dateHeader');
 
-// EL BOTON DE ENTER PARA AGREGAR
-const Enter = document.querySelector('#enter');
-
-//EL PARRAFO QUE CONTIENE EL INPUT DE DATE
-// const dateHomework = document.querySelector('.dateHomework');
+//PRIORIDAD
+const priorityElement = document.querySelector('#levelPriority');
 
 // LA LISTA
 const list = document.querySelector('#list');
@@ -14,19 +11,24 @@ const list = document.querySelector('#list');
 // EL INPUT DE TEXTO PARA ESCRIBIR LAS TAREAS
 const inputAdd = document.querySelector('#newHomework');
 
-// EL INPUT DEL CALENDARIO
-// const inputDate = document.querySelector('#date');
-
 // LAS CLASES DE LOS BOTONES PARA MARCAR COMO REALIZADO O NO
 const Check = 'fa-check-circle';
 const Uncheck = 'fa-circle';
+
 // LA CLASE DEL SUBRAYADO
 const lineThrough = 'line-through';
+
 // el id que hara que cambie
-let id;
-let LIST;
+let id = 0;
+let LIST = [];
+
+//FUNCION QUE GUARDA EN EL LOCAL STORAGE
+const addLocal = () => localStorage.setItem('todoList', JSON.stringify(LIST));
 
 const date = new Date();
+
+
+//Agregar al html la fecha del dia
 dateHeader.innerHTML = date.toLocaleDateString('en-madrid', {
     weekday: 'long',
     month: 'short',
@@ -34,17 +36,21 @@ dateHeader.innerHTML = date.toLocaleDateString('en-madrid', {
     year: 'numeric',
 });
 
-//CREAMOS UNA FUNCION PARA AGREGAR LAS TAREAS
-function addHomework(homework, id, check, uncheck) {
+//CREAMOS UNA FUNCION PRINCIPAL DONDE CREAREMOS LA INTERACCION DE TODAS LAS TAREAS QUE SE AGREGUEN
+function addHomework(homework, id, check, uncheck, date, priority) {
     if (uncheck) {
         return;
     }
+
+    const dateString = date.toLocaleDateString();
+
     const CHECK = check ? Check : Uncheck;
     const LineThrough = check ? lineThrough : '';
-    const element = `<li id="element> 
-                           <i id="${id}" data="check" class="far ${CHECK} " ></i>
+    const element = `<li id="${id}" class="todo-item ${priority}"> 
+                           <i data-id="${id}" data="check" class="far ${CHECK} " ></i>
                            <p class="text ${LineThrough} ">${homework} </p>
-                           <i data="remove" id="${id}" class="fa-solid fa-trash-can"></i> 
+                           <time datetime="${dateString}" class="todo-date">${dateString}</time>
+                           <i data="remove" data-id="${id}" class="fa-solid fa-trash-can"></i> 
                         </li>`;
     list.insertAdjacentHTML('beforeend', element);
 }
@@ -54,46 +60,44 @@ function checkHomework(element) {
     element.classList.toggle(Check);
     element.classList.toggle(Uncheck);
     element.parentNode.querySelector('.text').classList.toggle(lineThrough);
-    LIST[element.id].check = LIST[element.id].check ? false : true;
+    const index = element.getAttribute('data-id');
+    LIST[index].check = LIST[index].check ? false : true;
+    addLocal();
 }
 
-Enter.addEventListener('click', () => {
-    const homework = inputAdd.value;
-    if (homework) {
-        addHomework(homework, id, false, false);
-        LIST.push({
-            content: homework,
-            id: id,
-            check: false,
-            uncheck: false,
-        });
-    }
-    inputAdd.value = '';
-    id++;
-});
 // FUNCIÓN PARA ELIMINAR TAREAS
 function removeHomework(element) {
     element.parentNode.parentNode.removeChild(element.parentNode);
-    LIST[element.id].remove = true;
+    const index = element.getAttribute('data-id');
+    LIST[index].remove = true;
+    LIST.splice(index, 1);
+    addLocal();
 }
 
-document.addEventListener('keyup', function (e) {
-    if (e.key === 'Enter') {
-        const homework = inputAdd.value;
-        if (homework) {
-            addHomework(homework, id, false, false);
-            LIST.push({
-                content: homework,
-                id: id,
-                check: false,
-                uncheck: false,
-            });
-        }
-        inputAdd.value = '';
-        id++;
-    }
-});
+//FUNCION QUE CARGA EL LOCAL STORAGE
+function loadLocal() {
+    const todoList = JSON.parse(localStorage.getItem('todoList'));
 
+    if (todoList) {
+        LIST = todoList;
+
+        todoList.forEach((item) => {
+            const date = new Date(item.date);
+            addHomework(
+                item.content,
+                item.id,
+                item.check,
+                item.uncheck,
+                date,
+                item.priority
+            );
+        });
+    }
+}
+
+loadLocal();
+
+//EVENTOS
 list.addEventListener('click', function (e) {
     const element = e.target;
     const elementData = element.attributes.data.value;
@@ -103,4 +107,28 @@ list.addEventListener('click', function (e) {
         removeHomework(element);
     }
 });
-// GUARDAR EN LOCAL STOREGE GET ITEM
+
+document.getElementById('homework-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const homework = inputAdd.value;
+    if (homework) {
+        const priority = priorityElement.value;
+
+        const fecha = new Date();
+        addHomework(homework, id, false, false, fecha, priority);
+        LIST.push({
+            content: homework,
+            id: id,
+            check: false,
+            uncheck: false,
+            date: fecha,
+            priority: priority,
+        });
+    }
+    inputAdd.value = '';
+    id++;
+
+    addLocal();
+});
+
